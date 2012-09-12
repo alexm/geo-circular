c3473dfa73991a0c3d8e3b297f9eb3a28352b42b
Alex Muntada <alexm@alexm.org>
2012-09-12 22:38:34 +0200

    afegeixo els resultats i d'altres petits canvis
    *   genero la pàgina de resultats amb un CSV
    *   enllaç a la pàgina de resultats al menú
    *   poso el calendari i el widget de twitter a totes les pàgines
    *   moc els tracks de gps a la columna central

--- src/circular.pl
+++ src/circular.pl
@@ -1,5 +1,13 @@
 #!/usr/bin/env perl
+
+use Modern::Perl 2012;
+
 use Mojolicious::Lite;
+use Text::CSV;
+
+my %args = (
+    "circular/resultats" => parse_csv('resultats.csv'),
+);
 
 app->defaults( layout => 'default' );
 
@@ -7,7 +15,20 @@ chomp( my @pages = do { open my $fd, '<', 'PAGES' or die; <$fd> } );
 print STDERR "Rendering... @pages\n";
 
 for my $page (@pages) {
-    get "/$page" => sub { shift->render($page) };
+    get "/$page" => sub { shift->render( $page, args => $args{$page} ) };
 }
 
 app->start;
+
+sub parse_csv {
+    my ($filename) = @_;
+
+    my $csv = Text::CSV->new ( { binary => 1, sep_char => ';' } )  # should set binary attribute.
+        or die "Cannot use CSV: ".Text::CSV->error_diag ();
+
+    open my $fh, "<:encoding(utf8)", $filename or die "$filename: $!";
+    my $rows_ref = $csv->getline_all($fh);
+    close $fh;
+
+    return $rows_ref;
+}
